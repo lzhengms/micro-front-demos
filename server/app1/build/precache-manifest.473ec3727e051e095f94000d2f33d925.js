d9821c9345f9d482b0e73976622b73e2e5f2b712
lzhengms <2393408826@qq.com>
2020-09-25 13:25:58 +0800

    微前端入门学习的例子
    Change-Id: Id15fbaa160eb9afe342032bc27ba0d3437dd9dda

---
+++ server/app1/build/precache-manifest.473ec3727e051e095f94000d2f33d925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fff64e917446cf27993ee8a6079642a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b0468bd69df351f0bd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "144d76a323687f1fe71d",
+    "url": "/static/js/2.2532e564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2532e564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b0468bd69df351f0bd",
+    "url": "/static/js/main.4455dc9a.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server/app2/build/precache-manifest.997924d76f607f3997a89e6ccb7acefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859dc3988828649031a655f818327a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2609217e8fc8c34b921",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f8b48953b1be46bc0e5",
+    "url": "/static/js/2.8b854741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b854741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2609217e8fc8c34b921",
+    "url": "/static/js/main.bf5bcca3.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  }
+]);
